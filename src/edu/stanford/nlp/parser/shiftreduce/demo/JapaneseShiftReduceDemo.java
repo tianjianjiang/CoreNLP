354b037725ee3fa42a6c0753725da61d9b2dcad3
Mike Tian-Jian Jiang <tianjian@yaraku.com>
2016-04-06 14:25:54 +0900

    Simplified the demo.

--- src/edu/stanford/nlp/parser/shiftreduce/demo/JapaneseShiftReduceDemo.java
+++ src/edu/stanford/nlp/parser/shiftreduce/demo/JapaneseShiftReduceDemo.java
@@ -3,11 +3,13 @@ package edu.stanford.nlp.parser.shiftreduce.demo;
 import edu.stanford.nlp.ling.TaggedWord;
 import edu.stanford.nlp.parser.shiftreduce.ShiftReduceParser;
 import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.util.logging.Redwood;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import edu.stanford.nlp.util.logging.RedwoodConfiguration;
+import org.chasen.mecab.MeCabConstants;
 import org.chasen.mecab.Node;
 import org.chasen.mecab.Tagger;
 
@@ -19,8 +21,6 @@ import org.chasen.mecab.Tagger;
  * @author Mike Tian-Jian Jiang
  */
 public class JapaneseShiftReduceDemo  {
-
-    private static Redwood.RedwoodChannels log = Redwood.channels(JapaneseShiftReduceDemo.class);
 
     public static void main(String[] args) {
         String modelPath = "ja.beam.rightmost.model.ser.gz";
@@ -36,22 +36,32 @@ public class JapaneseShiftReduceDemo  {
             }
         }
 
-        String text = "太郎は二郎にこの本を渡した。";
-
         System.loadLibrary("mecab-java");
+
+        RedwoodConfiguration.empty().capture(System.err).apply();
         ShiftReduceParser model = ShiftReduceParser.loadModel(modelPath);
+        RedwoodConfiguration.current().clear().apply();
 
         Tagger tagger = new Tagger();
-        Node node = tagger.parseToNode(text);
-        List<TaggedWord> tagged = new ArrayList<>();
-        for (; node != null; node = node.getNext()) {
-            String word = node.getSurface();
-            String tag = node.getFeature().split(",")[0];
-            log.info(word);
-            log.info(tag);
-            tagged.add(new TaggedWord(word, tag));
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String text = scanner.nextLine();
+            Node node = tagger.parseToNode(text);
+            if (null == node) {
+                continue;
+            }
+            node = node.getNext();
+
+            List<TaggedWord> tagged = new ArrayList<>();
+            for (; node.getStat() != MeCabConstants.MECAB_EOS_NODE; node = node.getNext()) {
+                String word = node.getSurface();
+                String tag = node.getFeature().split(",")[0];
+                tagged.add(new TaggedWord(word, tag));
+            }
+            Tree tree = model.apply(tagged);
+            System.out.println(tree);
         }
-        Tree tree = model.apply(tagged);
-        log.info(tree);
+
     }
 }
