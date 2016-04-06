5b81c4fd62a89d535360478f2860699fac35c101
Mike Tian-Jian Jiang <tianjian@yaraku.com>
2016-04-06 18:56:02 +0900

    Added a dirty hack to read slash-delimited tagged words.

--- src/edu/stanford/nlp/parser/shiftreduce/demo/JapaneseShiftReduceDemo.java
+++ src/edu/stanford/nlp/parser/shiftreduce/demo/JapaneseShiftReduceDemo.java
@@ -24,6 +24,7 @@ public class JapaneseShiftReduceDemo  {
 
     public static void main(String[] args) {
         String modelPath = "ja.beam.rightmost.model.ser.gz";
+        boolean callMeCab = false;
 
         for (int argIndex = 0; argIndex < args.length; ) {
             switch (args[argIndex]) {
@@ -31,35 +32,50 @@ public class JapaneseShiftReduceDemo  {
                     modelPath = args[argIndex + 1];
                     argIndex += 2;
                     break;
+                case "-mecab":
+                    callMeCab = true;
+                    argIndex += 2;
+                    break;
                 default:
                     throw new RuntimeException("Unknown argument " + args[argIndex]);
             }
         }
 
-        System.loadLibrary("mecab-java");
-
         RedwoodConfiguration.empty().capture(System.err).apply();
         ShiftReduceParser model = ShiftReduceParser.loadModel(modelPath);
         RedwoodConfiguration.current().clear().apply();
 
-        Tagger tagger = new Tagger();
+        Tagger tagger = null;
+        if (callMeCab) {
+            System.loadLibrary("mecab-java");
+            tagger = new Tagger();
+            tagger.parse("太郎は二郎にこの本を渡した。");
+        }
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String text = scanner.nextLine();
-            Node node = tagger.parseToNode(text);
-            if (null == node) {
-                continue;
+            List<TaggedWord> taggedWords = new ArrayList<>();
+            if (callMeCab) {
+                Node node = tagger.parseToNode(text);
+                if (null == node) {
+                    continue;
+                }
+                node = node.getNext();
+                for (; node.getStat() != MeCabConstants.MECAB_EOS_NODE; node = node.getNext()) {
+                    String word = node.getSurface();
+                    String tag = node.getFeature().split(",")[0];
+                    taggedWords.add(new TaggedWord(word, tag));
+                }
+            } else {
+                String[] tokens = text.split(" ");
+                for (String token: tokens) {
+                    TaggedWord taggedWord = new TaggedWord();
+                    taggedWord.setFromString(token);
+                    taggedWords.add(taggedWord);
+                }
             }
-            node = node.getNext();
-
-            List<TaggedWord> tagged = new ArrayList<>();
-            for (; node.getStat() != MeCabConstants.MECAB_EOS_NODE; node = node.getNext()) {
-                String word = node.getSurface();
-                String tag = node.getFeature().split(",")[0];
-                tagged.add(new TaggedWord(word, tag));
-            }
-            Tree tree = model.apply(tagged);
+            Tree tree = model.apply(taggedWords);
             System.out.println(tree);
         }
 
